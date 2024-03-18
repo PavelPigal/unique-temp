a1650316526e250b83dcef737e2cb1455df9cb66
BurritC <raremirzanuda@gmail.com>
2024-03-18 20:01:42 +0100

    my commit

--- aaa.py
+++ aaa.py
@@ -5,5 +5,7 @@ print(n)
 
 import random
 print("hello world")
-[[random.randint(1,101) for _ in range(10)] for _ in range(10)]
-
+[[random.randint(1,101) for _ in range(10)] for _ in range(10)] #eto wto takoe? 
+#some information
+x = lambda row, cols: [[0] * cols for i in range(rows)]
+print(x)
