8a73c7b7736dd1cc923d8ddbda84c297b7b0a2e6
pavlinux <pavel.pigal@gmail.com>
2024-03-18 20:13:13 +0100

    my second

--- aaa.py
+++ aaa.py
@@ -7,3 +7,13 @@ import random
 print("hello world")
 [[random.randint(1,101) for _ in range(10)] for _ in range(10)]
 
+print(input("hello: "))
+print(f"{x}")
+
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+
